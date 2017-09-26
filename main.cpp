0ff5bdd6fe465a6c5e2e6aa495d41dc9b95122a9
MLaurenzo <laurent.bertholle@gmail.com>
2017-09-26 15:29:37 +0200

    Printed string class.

--- main.cpp
+++ main.cpp
@@ -5,21 +5,30 @@
 
 using namespace std;
 
+void print(const string& s)
+{
+    cout << s << endl;
+}
+
 class Wrapper {
 public:
     template<typename T>
     Wrapper(T object) : m_internal(make_unique<InternalImpl<T>>(object)) {}
 
-    void print() const {}
+    friend void print(const Wrapper& wrapper)
+    { wrapper.m_internal->print_(); }
 
 private:
     struct InternalInterface {
         virtual ~InternalInterface() = default;
+        virtual void print_() const = 0;
     };
     template<typename T>
     struct InternalImpl : InternalInterface
     {
         InternalImpl(T object) : m_object(object) {}
+        void print_() const override
+        { print(m_object); }
 
     private:
         T m_object;
@@ -33,6 +42,6 @@ int main()
     v.emplace_back(string("I am using string class."));
 
     for_each(v.begin(), v.end(),
-             [](const Wrapper& e) { e.print(); }
+             [](const Wrapper& e) { print(e); }
     );
 }
